5bb897d6167377652782047150e34465474f2749
Kristjan Jansen <kristjan.jansen@gmail.com>
2021-01-26 11:56:22 +0200

    Adding slot example (#2)

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,11 +1,19 @@
 <template>
     <h3>Hello from VueJS component <code>Users</code></h3>
+
     <p>Got users data from prop <code>usersFromProps</code></p>
-    <pre v-for="(user, i) in usersFromProps" :key="i">
+
+    <pre v-for="(user, i) in usersFromProps" :key="i" @click="onClick">
 Name: {{ user.name }}</pre
     >
+
     <p>Got users data from API route <code>/api/users</code></p>
-    <pre v-for="(user, i) in usersFromApi" :key="i">Name: {{ user.name }}</pre>
+
+    <pre v-for="(user, i) in usersFromApi" :key="i" @click="onClick">
+Name: {{ user.name }}</pre
+    >
+
+    <slot :usersFromProps="usersFromProps" :onClick="onClick" />
 </template>
 
 <script setup>
@@ -21,4 +29,6 @@ const usersFromApi = ref([]);
 fetch("./api/users")
     .then((res) => res.json())
     .then((res) => (usersFromApi.value = res));
+
+const onClick = () => alert("clicked");
 </script>
